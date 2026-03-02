70141409e6f1a3fe3dc066e9bc582a6a35920f98
Brandon <68079496+brandon-1725@users.noreply.github.com>
2026-03-01 20:52:40 -0500

    Wrote main method and updated kernel methods for p-o-f
    Added a main method to show the component in action. Changed from ints to doubles to better represent stats. Used decimal format to properly format doubles when printing out stats. Updated private members of class to represent the players name. Updated kernel methods to represent the new private member. Corrected getStatAndValue, average, least, and greatest StatValue methods.

--- src/FantasyFootballDatabase.java
+++ src/FantasyFootballDatabase.java
@@ -1,7 +1,10 @@
 import components.map.Map;
+import components.map.Map.Pair;
 import components.map.Map1L;
 import components.sequence.Sequence;
 import components.sequence.Sequence1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
 
 /**
  * Fanstasy Football Database for Portfolio Project.
@@ -11,22 +14,24 @@ public class FantasyFootballDatabase {
      * Private members ---------------------------------------------------------
      */
 
+    private String playerName;
+
     /**
      * Map of stat to values.
      */
-    private Map<String, Sequence<Integer>> mapOfStatNameToValues;
+    private Map<String, Sequence<Double>> mapOfStatNameToValues;
 
     /**
      * Database.
      */
-    private Map<String, Map<String, Sequence<Integer>>> database;
+    private Map<String, Map<String, Sequence<Double>>> database;
 
     /**
      * Creator of inital representation.
      */
     private void createNewRep() {
-        this.mapOfStatNameToValues = new Map1L<String, Sequence<Integer>>();
-        this.database = new Map1L<String, Map<String, Sequence<Integer>>>();
+        this.mapOfStatNameToValues = new Map1L<String, Sequence<Double>>();
+        this.database = new Map1L<String, Map<String, Sequence<Double>>>();
     }
 
     /*
@@ -41,13 +46,6 @@ public class FantasyFootballDatabase {
     }
 
     /*
-     * Standard methods --------------------------------------------------------
-     */
-
-    // not important for proof of concept
-    // TODO write newInstance, clear, transferFrom
-
-    /*
      * Kernel methods ----------------------------------------------------------
      */
 
@@ -58,6 +56,7 @@ public class FantasyFootballDatabase {
      *            name of player entered into database.
      */
     public final void enterPlayer(String name) {
+        this.playerName = name;
         this.database.add(name, this.mapOfStatNameToValues);
     }
 
@@ -68,9 +67,9 @@ public class FantasyFootballDatabase {
      * @param value
      * @requires Name in database
      */
-    public final void enterStat(String stat, int value) {
-        Sequence<Integer> statEntries = new Sequence1L<Integer>();
-        statEntries.add(statEntries.length() - 1, value);
+    public final void enterStat(String stat, double value) {
+        Sequence<Double> statEntries = new Sequence1L<Double>();
+        statEntries.add(statEntries.length(), value);
 
         if (this.mapOfStatNameToValues.hasKey(stat)) {
             this.mapOfStatNameToValues.value(stat).add(statEntries.length() - 1,
@@ -78,6 +77,7 @@ public class FantasyFootballDatabase {
         } else {
             this.mapOfStatNameToValues.add(stat, statEntries);
         }
+
     }
 
     /**
@@ -86,9 +86,13 @@ public class FantasyFootballDatabase {
      * @param stat
      * @return map of stats and values for the stats
      */
-    public final Map.Pair<String, Map<String, Sequence<Integer>>> getStatAndValue(
+    public final Map.Pair<String, Sequence<Double>> getStatAndValue(
             String stat) {
-        return this.database.remove(stat);
+
+        Map.Pair<String, Sequence<Double>> statAndValues = this.database
+                .value(stat).remove(stat);
+        this.database.add(this.playerName, this.mapOfStatNameToValues);
+        return statAndValues;
     }
 
     /*
@@ -97,72 +101,112 @@ public class FantasyFootballDatabase {
 
     /**
      * Averages out the stat.
-     * 
+     *
      * @param stat
      * @return the average of the stat
      */
-    public final int averageStatValue(String stat) {
-        int valuesOfStat = 0;
+    public final double averageStatValue(String stat) {
+        double valuesOfStat = 0;
 
-        Map.Pair<String, Map<String, Sequence<Integer>>> statsAndValues = this
-                .getStatAndValue(stat);
-        Sequence<Integer> statValues = statsAndValues.value().value(stat);
+        Pair<String, Map<String, Sequence<Double>>> statsAndValues = this.database
+                .remove(this.playerName);
+        Sequence<Double> statValues = statsAndValues.value().value(stat);
 
-        for (int statValue : statValues) {
+        for (double statValue : statValues) {
             valuesOfStat += statValue;
         }
+
+        this.database.add(this.playerName, this.mapOfStatNameToValues);
 
         return valuesOfStat / statValues.length();
     }
 
     /**
      * Least stat value.
-     * 
+     *
      * @param stat
      * @return the smallest value of the stat
      */
-    public final int leastStatValue(String stat) {
-        Map.Pair<String, Map<String, Sequence<Integer>>> statsAndValues = this
-                .getStatAndValue(stat);
-        Sequence<Integer> statValues = statsAndValues.value().value(stat);
+    public final double leastStatValue(String stat) {
+        Pair<String, Map<String, Sequence<Double>>> statsAndValues = this.database
+                .remove(this.playerName);
+        Sequence<Double> statValues = statsAndValues.value().value(stat);
 
-        int leastStatValue = statValues.entry(0);
+        double leastStatValue = statValues.entry(0);
         for (int i = 0; i < statValues.length(); i++) {
             if (statValues.entry(i) < leastStatValue) {
                 leastStatValue = statValues.entry(i);
             }
         }
+        this.database.add(this.playerName, this.mapOfStatNameToValues);
 
         return leastStatValue;
     }
 
     /**
      * Greatest stat value.
-     * 
+     *
      * @param stat
      * @return the greatest value of the stat
      */
-    public final int greatestStatValue(String stat) {
-        Map.Pair<String, Map<String, Sequence<Integer>>> statsAndValues = this
-                .getStatAndValue(stat);
-        Sequence<Integer> statValues = statsAndValues.value().value(stat);
+    public final double greatestStatValue(String stat) {
+        Pair<String, Map<String, Sequence<Double>>> statsAndValues = this.database
+                .remove(this.playerName);
+        Sequence<Double> statValues = statsAndValues.value().value(stat);
 
-        int greatestStatValue = statValues.entry(0);
+        double greatestStatValue = statValues.entry(0);
         for (int i = 0; i < statValues.length(); i++) {
             if (statValues.entry(i) > greatestStatValue) {
                 greatestStatValue = statValues.entry(i);
             }
         }
+        this.database.add(this.playerName, this.mapOfStatNameToValues);
 
         return greatestStatValue;
     }
 
-    /**
-     * Main method.
-     *
-     * @param args
-     */
-    public void main(String[] args) {
+}
 
-    }
+/**
+ * Main method.
+ *
+ * @param args
+ */
+public void main(String[] args) {
+
+    // Did research and choose this
+    // #.0 means that the result of the format is all the digits before the
+    // decimal point, then at max one digit with a 0 showing if there is no
+    // digits after decimal point.
+    // 14.0 is formatted as 14.0 while 14.2576 is formatted as 14.3
+    // Choose just one number after the decimal point because ESPN NFL stats
+    // only show one number after decimal point for averages
+    DecimalFormat df = new DecimalFormat("#.0");
+
+    SimpleWriter out = new SimpleWriter1L();
+    FantasyFootballDatabase quarterback = new FantasyFootballDatabase();
+    quarterback.enterPlayer("Player1");
+    quarterback.enterStat("yards", 100);
+    quarterback.enterStat("yards", 106);
+    quarterback.enterStat("yards", 300);
+
+    out.println(quarterback.playerName + " passed for "
+            + df.format(quarterback.averageStatValue("yards"))
+            + " yards per game on average in this season.");
+    out.println("The least amount of yards the quaterback passed for was "
+            + df.format(quarterback.leastStatValue("yards"))
+            + " yards in a game this season.");
+
+    quarterback.enterStat("TD", 0);
+    quarterback.enterStat("TD", 4);
+    quarterback.enterStat("TD", 0);
+
+    out.println(quarterback.playerName + " passed for "
+            + df.format(quarterback.averageStatValue("TD"))
+            + " TDs per game on average in this season.");
+    out.println("The most touchdowns the quaterback passed for was "
+            + df.format(quarterback.greatestStatValue("TD"))
+            + " TDs in a game this season.");
+
+    out.close();
 }
